08c52b8d451863cc6ad2b474207a4284d95b872a
sheninthjr <sheninthjr23@gmail.com>
2023-11-11 20:58:15 +0530

    Email added

---
+++ apps/client/emails/Welcome.tsx
@@ -0,0 +1,16 @@
+
+import { Html,Button } from '@react-email/components'
+import * as React from "react";
+
+export default function Email() {
+  return (
+    <Html>
+      <Button
+        href="https://example.com"
+        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+}
