511b8784afa618106809ac71e925173f1251a4b7
sheninthjr <sheninthjr23@gmail.com>
2023-11-13 17:30:31 +0530

    Resend Domain Name

--- apps/client/app/api/email/route.tsx
+++ apps/client/app/api/email/route.tsx
@@ -2,13 +2,13 @@ import Welcome from "@/emails/Welcome";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("re_BTkbkGmj_fWZCxzki9aqvDez3wRysz1fy");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   const data = await resend.emails.send({
-    from: "todo.sheninthjr.com",
-    to: "sheninthjr@gmail.com",
-    subject: "Hello World",
+    from: "sheninthjr.com",
+    to: "sheninthjr23@gmail.com",
+    subject: "Hey",
     react: <Welcome />,
   });
   return NextResponse.json({
